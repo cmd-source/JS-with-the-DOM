ca1eac78f1f5262a0555d21c07d388ef59fac73f
cmd-source <conorcrosbie09@gmail.com>
2021-02-24 14:36:25 +0000

    adding a timeout

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 var xhr = new XMLHttpRequest();
+var data;
 
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/")
 xhr.send();
 
+function setData(jsonData){
+    data = jsonData;
+}
+
 xhr.onreadystatechange = function(){
     if (this.readyState == 4 && this.status == 200){
-        document.getElementById("data").innerHTML= xhr.responseText;
-        console.log(JSON.parse(this.responseText))
+        data = JSON.parse(this.responseText)
     }
 };
 
+
+setTimeout(function(){
+    console.log(data)
+}, 500)
